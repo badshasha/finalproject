8a726913a5eb7ecc29fc9fd379f34202beefc88d
shavendra <shavendragoesoft@gmail.com>
2021-10-04 13:13:20 +0530

    add subproject apps

---
+++ adminController/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class AdmincontrollerConfig(AppConfig):
+    name = 'adminController'
---
+++ crudinfo/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class CrudinfoConfig(AppConfig):
+    name = 'crudinfo'
